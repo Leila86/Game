6b9f3e76983c44216df8abd3a1a0481c62838ec5
andberg <andreaberglund@hotmail.com>
2013-09-17 15:42:30 +0200

    added all files

---
+++ game.js
@@ -0,0 +1,9 @@
+/*jslint browser:true */
+/*global alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+alert("One day, Bowser invades the Mushroom Kingdom. You are the only one who can stop him by saving the prince from the dungeon in Moria.");
+var que1 = prompt("Do you choose to take on this battle of the minds?");
+if (que1 === "yes" || que1 === "ja") {
+    console.log("Welcome to this epic adventure!");
+} else {
+    console.log("Game over; you are such a loser!");
+}
---
+++ stort
@@ -0,0 +1,3 @@
+
+One day, Bowser invades the Mushroom Kingdom. You are the only one who can stop him by saving the prince from the dungeon in Moria. 
+Do you choose to take on this battle of the minds? 
