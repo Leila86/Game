4ab74624db20046ffb24929535c9e6dc67f566ff
andberg <andreaberglund@hotmail.com>
2013-10-03 09:21:51 +0200

    Added new Memory Game

--- game.play.js
+++ game.play.js
@@ -1,12 +1,13 @@
 /*jslint browser:true */
 /*global game: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
-var i, answer, currentQuestion, points = 0,
-    question;
+
 
 game.play = function () {
     "use strict";
+    var i, answer, currentQuestion, points = 0,
+        questionCount = game.questions.length;
     alert("One day, Bowser invades and puts a spell on the Mushroom Kingdom which turned its inhabitants into blocks, weeds, and other weird things. You are the only one who can stop him by earning more points than Bowser in this battle of the minds!");
-    for (i = 0; i < game.questions.length; i = i + 1) {
+    for (i = 0; i < questionCount; i = i + 1) {
         currentQuestion = game.logic.getQuestions();
         answer = game.ui.askQuestion(currentQuestion);
         game.logic.isCorrect(currentQuestion, answer);
