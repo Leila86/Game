9b64cb194090ae804534e1e6ce06ae26b7babe79
andberg <andreaberglund@hotmail.com>
2013-10-11 11:59:54 +0200

    fixat med play-filen

--- Memorygame/js/memory.play.js
+++ Memorygame/js/memory.play.js
@@ -4,134 +4,45 @@
 $(function () {
 
     "use strict";
-    var checkCards,
-        card1,
+    var card1,
+        div2,
         card2,
-        collectI = [],
-        collectImg = [],
+        div1,
         countClick = 0,
         points = 0;
 
     $(".frame div").click(function () {
         var i = Number($(this).attr("id").replace("div", "")); // Här får vi ut vilket nummer Div:en som vi klickat på har. 
         $(this).css("background-image", "url(" + memory.array[i].url + ")"); // Här tilldelar vi den diven en bild från bild arrayen. Bilden har samma indexnummer som diven. 
-        countClick = countClick + 1;
-        collectI.push(i); // Vi sparar vilka divs i:n som vi klickat på i en  array utanför klick-funktionen.
-        collectImg.push(memory.array[i].n); //Sparar vilka nummer som bilden har i array utanför klick-funktionen.
+        if (countClick === 0) {
+            countClick = countClick + 1;
+            div1 = $(this);
+            card1 = memory.array[i].url;
+            //return card1 && div1;
+        } else {
+            countClick = countClick + 1;
+            div2 = $(this);
+            card2 = memory.array[i].url;
+            if (card1 !== card2) {
+                setTimeout(function () {
+                    div1.css("background-image", "url(../images/monster.jpg)");
+                    div2.css("background-image", "url(../images/monster.jpg)");
+                }, 2000);
+                countClick = 0;
+            }
+        }
     });
 
-    //Hit fungerar allt bra!
-
-    checkCards = function (collectImg, collectI) {
-        card1 = ("div" + collectI[0].toString);
-        card2 = ("div" + collectI[1].toString);
-        if ((countClick === 2) && (collectImg[0] === collectImg[1])) {
-            points = points + 1;
-        } else {
-            $(card2, card1).css("background-image", "images/monster.jpg");
-        }
-    };
     /*
-Tanken med funktionen ovan är att vi ska kolla om collectImg:s index 0 och 1 är samma,
-det innebär att de är ett par och ska fortsätta vara öppna. 
-Vi lade även in countClick i denna funktion, men jag vet inte om den kommer finnas kvar här,
-eller kanske bli en egen. 
 
-Problemet är att det inte fungerar när jag provkör i console. 
-Och js-lint klagar på att checkCards är oanvänt, vilket den ju inte är?
-
-
-Om jag kan få lite tips på varför det inte vill sig så är jag tacksam, och 
-hur jag ska kunna testa det i consolen. 
+1) När man click första gång spara kortets image url till variable card1.
+2) När man click andra gången
+     2a)  spara kortets image url till variable card2.
+     2b) gemföra card1 and card2
+     2c) om annurlunder visa baksidan av kort igen
+     2d) noll ställ klickräknare så nästa klick bli tolkad som klick 1 igen.
 
 
 */
 
 });
-
-
-
-/*
-$(function () {
-    "use strict";
-    var points = 0,
-        checkAnswer, writeQuestion, correctAnswer,
-        questions = [{
-            html: "What is the longest river in the world is called?",
-            answers: ["Nile", "Amazon", "Thames"],
-            correctAnswer: "Nile"
-        }, {
-            html: "What is the highest mountain in the world called?",
-            answers: ["Everest", "Mount Blanc", "Snowden"],
-            correctAnswer: "Everest"
-        }, {
-            html: "The longest road in the world is approximately how long?",
-            answers: ["20000km", "2000km", "8000km"],
-            correctAnswer: "8000km"
-        }, {
-            html: "Which country grows the most coffee",
-            answers: ["Sweden", "Brazil", "France"],
-            correctAnswer: "Brazil"
-        }, {
-            html: "Which sea or ocean does the river Jordan run into?",
-            answers: ["Pacific", "Atlantic", "Dead Sea"],
-            correctAnswer: "Atlantic"
-        }],
-        currentQuestionIndex = 0;
-    
-    writeQuestion = function () {
-        $("#questionCount").text(questions.length);
-
-        $("#questionNumber").text(currentQuestionIndex);
-        
-        $("#questionText").text(questions[currentQuestionIndex].html);
-        
-        $("#buttonA").text(questions[currentQuestionIndex].answers[0]);
-        $("#buttonB").text(questions[currentQuestionIndex].answers[1]);
-        $("#buttonC").text(questions[currentQuestionIndex].answers[2]);
-
-        correctAnswer = questions[currentQuestionIndex].correctAnswer;
-    };
-    checkAnswer = function (selectedAnswer) {
-        var moveToNextQuestion = function () {
-            currentQuestionIndex = currentQuestionIndex + 1;
-
-            $("#result").text("You got " + points + " points!");
-
-            if (currentQuestionIndex < questions.length) {
-                writeQuestion();
-            } else {
-                $("#result").show();
-                setTimeout(function () {
-                    $("#result").hide();
-                }, 10000);
-                currentQuestionIndex = 0;
-                writeQuestion();
-            }
-        };
-
-    
-        if (selectedAnswer === correctAnswer) {
-            points = points + 1;
-            $("#correctAnswer").show();
-            setTimeout(function () {
-                $("#correctAnswer").hide();
-            }, 1000);
-            moveToNextQuestion();
-        } else {
-            $("#wrongAnswer").show();
-            setTimeout(function () {
-                $("#wrongAnswer").hide();
-            }, 1000);
-            moveToNextQuestion();
-        }
-
-    };
-
-
-    writeQuestion();
-    $("#buttons div").click(function () {
-        checkAnswer($(this).text());
-    });
-
-});*/
