9797d62a8d3ae8f85d5cca35273916116aeafc28
andberg <andreaberglund@hotmail.com>
2013-10-18 11:51:25 +0200

    Cleaned some code

--- Memorygame/js/memory.array.js
+++ Memorygame/js/memory.array.js
@@ -2,30 +2,21 @@
 /*global $: false, memory: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
 
 memory.array = _.shuffle([{
-    url: "images/1.jpg",
-    n: 1
+    url: "images/1.jpg"
 }, {
-    url: "images/2.jpg",
-    n: 2
+    url: "images/2.jpg"
 }, {
-    url: "images/3.jpg",
-    n: 3
+    url: "images/3.jpg"
 }, {
-    url: "images/4.jpg",
-    n: 4
+    url: "images/4.jpg"
 }, {
-    url: "images/1.jpg",
-    n: 1
+    url: "images/1.jpg"
 }, {
-    url: "images/2.jpg",
-    n: 2
+    url: "images/2.jpg"
 }, {
-    url: "images/3.jpg",
-    n: 3
+    url: "images/3.jpg"
 }, {
-    url: "images/4.jpg",
-    n: 4
+    url: "images/4.jpg"
 }, {
-    url: "images/jack.jpg",
-    n: 5
+    url: "images/jack.jpg"
 }]);
